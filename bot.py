0943ae199662de7293f7e464122764a8d5426d7a
5t4t1ck <dsaavedra88@gmail.com>
2016-02-26 13:52:37 -0500

    Revocando servicios de OAuth Tool

--- bot.py
+++ bot.py
@@ -3,10 +3,10 @@
 
 from twython import Twython
 
-CUSTOMER_KEY = "zEJRjkoAtVcEw0iloNuo35exu"
-CUSTOMER_SECRET= "KcDPquKp6TbDE8ywLl2cnfSJp1cs3dSGu7mE1uq1HDuymmhr2g"
-ACCESS_TOKEN = "52524273-nLPFENqt0YObSBnwRJwC6lr7dUAjwTVIWA5uhF0qq"
-ACCESS_TOKEN_SECRET = "7AecUJRNkUL70igO1Nhw9O1w1NQk5qPKfxfcjyjRmqK32"
+CUSTOMER_KEY = ""
+CUSTOMER_SECRET= ""
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET = ""
 
 twitter = Twython(CUSTOMER_KEY, 
                   CUSTOMER_SECRET, 
