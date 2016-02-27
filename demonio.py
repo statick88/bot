d74f19e63c1a865d59337d6ccfd625f6a0471137
5t4t1ck <dsaavedra88@gmail.com>
2016-02-26 19:21:13 -0500

    Quitando el time

--- demonio.py
+++ demonio.py
@@ -1,5 +1,5 @@
 from twython import Twython
-import time
+import random
 import os
 
 # Claves de la aplicacion Twitter
@@ -11,27 +11,19 @@ oauth_token_secret = ""
 twitter = Twython(app_key, app_secret, oauth_token, oauth_token_secret)
 
 def leertxtenlista():
- 
+
  # Bucle infinito
- while True:
-  
-  mensaje = ""
-  
-  # Abrimos el archivo
-  archi=open('twit','r')
-  lineas=archi.readlines()
-  
-  # Bucle para recorrer todas las entradas
-  for li in lineas:
-   time.sleep(0.01)
-   mensaje = li
-   # Agrega el Tweet
-   twitter.update_status(status=mensaje)
-   # Lapso de tiempo entre Tweets
-   time.sleep(600)
-   
-  archi.close()
-  
- 
+ mensaje = ""
+
+ # Abrimos el archivo
+ archi=open('twit','r')
+ lineas=archi.readlines()
+
+ # Elegir un twit aleatorio
+ t = random.randint(0,len(lineas)-1)
+ mensaje = lineas[t]
+ # Agrega el Tweet
+ twitter.update_status(status=mensaje)
+ archi.close()
 
 leertxtenlista()
